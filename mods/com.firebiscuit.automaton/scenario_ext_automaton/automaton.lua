f549c84795c7613b68e3bb57301e71b60f5ba880
Yohan <yohan@firebiscuit.com>
2025-12-21 16:37:51 +0900

    sample lua

--- mods/com.firebiscuit.automaton/scenario_ext_automaton/automaton.lua
+++ mods/com.firebiscuit.automaton/scenario_ext_automaton/automaton.lua
@@ -10,15 +10,6 @@
 ---@param context Context: script execution context, also provides R/W access to mod state. See format in docs.
 ---@param gameState GameState: Mutable GameState. See format in docs Any changes to it will be read by the game after execution of this function.
 function OnTurnStarts(context, gameState)
-    local numRedCrystals = 0
-    for _, crystal in ipairs(gameState.Player.CrystalsInInventory) do
-        if crystal.GUID == "Red" then
-            numRedCrystals = numRedCrystals + 1
-        end
-    end
-    local numWounds = math.max(1, numRedCrystals)
-    print("Player is bleeding " .. numWounds .. " cards")
-    for i = 1, numWounds do
-        gameState.Player.AcquireCard(GameValues.CardWoundId, GameValues.CardLocationHand)
-    end
+    print("Player is bleeding")
+    gameState.Player.AcquireCard(GameValues.CardWoundId, GameValues.CardLocationHand)
 end
